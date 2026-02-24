30fea52301fb6b0a2360ce5cd6e60124fb7190cc
sayooj45 <sayooj4545@gmail.com>
2026-02-24 11:33:36 +0530

    deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     port:3000,
     proxy:{
       '/api':{
-        target:'http://localhost:8000/',
+        target:'https://hospitalappointmentbookingsystem-backend.onrender.com',
         changeOrigin:true,
         rewrite:(path)=>path.replace(/^\/api/,"")
       }
